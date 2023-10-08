928dac3fb8ec76960995511fbe955f89e18d06c5
abegacheva <144003753+abegacheva@users.noreply.github.com>
2023-10-08 21:27:37 +0300

    Add files via upload

---
+++ 5/js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const productSelect = document.getElementById("product");
+    const quantityInput = document.getElementById("quantity");
+    const calculateBtn = document.getElementById("calculate-btn");
+    const result = document.getElementById("result");
+
+
+    const prices = {
+        product1: 10,
+        product2: 20,
+        product3: 30,
+    };
+
+    if(calculateBtn) {
+        calculateBtn.addEventListener("click", function () {
+            const selectedProduct = productSelect.value;
+            const quantity = parseInt(quantityInput.value);
+    
+            // Проверка на корректность ввода данных
+            const validQuantity = /^[1-9]\d*$/.test(quantityInput.value);
+    
+            if (!validQuantity) {
+                result.textContent = "Введите корректное количество товара (положительное целое число).";
+            } else if (selectedProduct in prices) {
+                const price = prices[selectedProduct];
+                const totalCost = price * quantity;
+                result.textContent = `Стоимость заказа: $${totalCost}`;
+            } else {
+                result.textContent = "Выберите товар из списка.";
+            }
+        });
+    }
+
+});
